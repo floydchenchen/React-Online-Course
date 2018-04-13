9103875a5f77a4866c9567b46965e3d3daa12b16
陈士豪 <>
2018-04-13 07:22:40 -0500

    updated jquery version

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDd7g6xFn_OPJn0fP50gJM3aMHa5HnwV2o';
 
+YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+    console.log(data);
+});
 // create a new component. This component should produce some HTML
 const App = () => {
     return (
